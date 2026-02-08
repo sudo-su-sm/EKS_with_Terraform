f361ee6b612e807854c3c5b9d50fec8a4db8b7c8
sudo-su-sm <161118580+sudo-su-sm@users.noreply.github.com>
2026-02-08 21:27:08 +0530

    Add Terraform S3 backend and state locking
    Add backend Terraform configuration: AWS provider, an S3 bucket (s3-backend-bucket-terraform-eks) with versioning enabled and lifecycle.prevent_destroy, and a DynamoDB table (terraform-lock-table) for state locking (PAY_PER_REQUEST) also protected with prevent_destroy. Include outputs for the S3 bucket and DynamoDB table names, and add region variables (default us-east-1) in both backend and root variable files.

---
+++ backend/main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "s3-backend-bucket-terraform-eks"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+    bucket = aws_s3_bucket.terraform_state.id
+    versioning_configuration {
+      status = "Enabled"
+    }
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name         = "terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+  
+}
+
+
+
