8408ab2df1e19447ba80966cfc73e7f6d8e5fdf0
sudo-su-sm <161118580+sudo-su-sm@users.noreply.github.com>
2026-02-09 17:24:03 +0530

    Add EKS cluster IAM role and attachment

---
+++ module/eks/main.tf
@@ -0,0 +1,20 @@
+resource "aws_iam_role" "cluster" {
+  name = "${var.cluster_name}-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
+}
